e2jk/trello-team-sync

commit 4a983f1983fbc8069c03f311eafb3b38228ce7dc

Inline add_checklistitem_to_checklist()

trello-team-sync.py
@@ -39,12 +39,6 @@ def remove_teams_checklist(config, master_card):
             logging.debug("Deleting checklist %s (%s) from master card %s" %(c["name"], c["id"], master_card["id"]))
             perform_request(config, "DELETE", "checklists/%s" % (c["id"]))
 
-def add_checklistitem_to_checklist(config, checklist_id, item_name):
-    logging.debug("Adding new checklistitem %s to checklist %s" % (item_name, checklist_id))
-    new_checklistitem = perform_request(config, "POST", "checklists/%s/checkItems" % checklist_id, {"name": item_name})
-    logging.debug(new_checklistitem)
-    return new_checklistitem
-
 def add_checklist_to_master_card(config, master_card):
     logging.debug("Creating new checklist")
     new_checklist = perform_request(config, "POST", "cards/%s/checklists" % master_card["id"], {"name": "Involved Teams"})
@@ -275,7 +269,9 @@ def process_master_card(config, master_card):
         if create_checklist:
             cl = add_checklist_to_master_card(config, master_card)
             for sb in slave_boards:
-                add_checklistitem_to_checklist(config, cl["id"], sb["name"])
+                logging.debug("Adding new checklistitem %s to checklist %s" % (sb["name"], cl["id"]))
+                perform_request(config, "POST", "checklists/%s/checkItems" % cl["id"], {"name": sb["name"]})
+                logging.debug(new_checklistitem)
 
         #TODO: Mark checklist item as Complete if slave card is Done
 

commit a6dced3f633caa3acb3f64e8322885917c199d8e

Fix debug statement (Issue introduced in 4a983f1983fbc8069c03f311eafb3b38228ce7dc)

trello-team-sync.py
@@ -260,7 +260,7 @@ def process_master_card(config, master_card):
             logging.debug(cl)
             for sb in slave_boards:
                 logging.debug("Adding new checklistitem %s to checklist %s" % (sb["name"], cl["id"]))
-                perform_request(config, "POST", "checklists/%s/checkItems" % cl["id"], {"name": sb["name"]})
+                new_checklistitem = perform_request(config, "POST", "checklists/%s/checkItems" % cl["id"], {"name": sb["name"]})
                 logging.debug(new_checklistitem)
 
         #TODO: Mark checklist item as Complete if slave card is Done